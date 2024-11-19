92b176b4e6b939cf44f99ea61dd26df425a84452
BiplobSorder <biplob.code.bd@gmail.com>
2024-11-19 16:29:00 +0600

    this is a tic_tac_toe game using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) return squares[a]
+  }
+  return false
+
+}
+
+function Square({ value, onSquareClick }) {
+
+
+  return <button onClick={onSquareClick} className="square">{value}</button>
+}
+
+function Board({ xIsNext, onPlay, squares }) {
+
+
+
+
+  
+
+
+
+
+  function handelClick(i) {
+    if (squares[i] || calculateWinner(squares)) return
+    const nextSquares = squares.slice()
+    if (xIsNext) {
+
+      nextSquares[i] = 'X'
+    } else {
+      nextSquares[i] = 'O'
+    }
+
+    onPlay(nextSquares)
+
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+      <Square value={squares[0]} onSquareClick={() => handelClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handelClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handelClick(2)} />
+
+      <Square value={squares[3]} onSquareClick={() => handelClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handelClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handelClick(5)} />
+
+
+      <Square value={squares[6]} onSquareClick={() => handelClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handelClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handelClick(8)} />
+
+    </>
+  );
+}
+
+export default function Game() {
+
+
+
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const [currentMove, setCurrentMove] = useState(0)
+  const xIsNext = currentMove % 2 === 0
+  const currentSquares = history[currentMove]
+
+  const winner = calculateWinner(currentSquares)
+  let status
+
+  if (winner) {
+    status = `Winner: ${winner}`
+
+  } else {
+    status = `Next Player: ${xIsNext ? 'X' : 'O'}`
+  }
+
+
+
+
+  function handlePlay(nextSquare) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquare]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
+
+  }
+
+  function jumpTo(move) {
+
+    setCurrentMove(move)
+
+
+  }
+
+
+
+  const moves = history.map((square, move) => {
+    let description
+
+
+    if (move > 0) {
+      description = `Go to move #${move}`
+    } else {
+      description = `Go to game start`
+    }
+
+
+
+
+    return <>
+      <li key={move}>
+        <button className='btn' onClick={() => { jumpTo(move) }}>{description}</button>
+      </li>
+    </>
+
+  })
+
+
+  return (
+    <div className="game">
+      <div className='section-1'>
+       <h1>{status}</h1>
+      </div>
+      <div className='section-2'>
+
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
+      </div>
+
     </div>
   );
 }
 
-export default App;
